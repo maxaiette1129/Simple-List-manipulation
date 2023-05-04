951e2d97e06a4b81cac389f0b16e83e0c07cb995
maxaiette1129 <maxaiettec@gmail.com>
2023-05-04 20:49:28 +0800

    Add project files.

---
+++ ArraysManipulation/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace ArraysManipulation
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            List<string> names = new List<string>();
+            List<string> password = new List<string>();
+            string uname;
+            string upassword;
+
+            Console.WriteLine("Enter username: ");
+            names.Add(Console.ReadLine());
+            Console.WriteLine("Enter password: ");
+            password.Add(Console.ReadLine());
+
+            Console.WriteLine("Enter Username");
+            uname = Console.ReadLine();
+            Console.WriteLine("Enter Password");
+            upassword = Console.ReadLine();
+
+            if(names.Contains(uname) && password[names.IndexOf(uname)] == upassword) 
+            {
+                Console.WriteLine("Login");
+            }
+            else 
+            {
+                Console.WriteLine("Error");
+            }
+            
+            
+            
+
+            
+        }
+    }
+}
